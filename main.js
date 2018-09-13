2105031716bfa58a31d1a4b341598c5c21b196b4
zoey <echohyt@sohu.com>
2018-09-13 22:45:54 +0800

    demo 1.2

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 let hash = [
   ["~","1","2","3","4","5","6","7","8","9","0","-","=","delete"],
   ["tab","Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P","{","}","|"],
-  ["caps","A", "S", "D", "F", "G", "H", "J", "K", "L",":","\"","return"],
+  ["caps","A", "S", "D", "F", "G", "H", "J", "K", "L",";","\'","return"],
   ["shift","Z", "X", "C", "V", "B", "N", "M",",",".","/","shift"]
 ];
-let hash2 = [['`','!','@','#','$','%','^','&','*','(',')','_','+'],['[',']','|'],['lock','enter'],['<','>','?']];
+
 let webs = {};
 
 let websInLocalStorage = JSON.parse(localStorage.getItem("webs") || "null");
@@ -14,17 +14,20 @@ if (websInLocalStorage) {
 
 let index1 = 0;
 while (index1 < hash.length) {
-  let ol1 = document.createElement("ol");
-  ol1.className = 'row'
+  let ul1 = document.createElement("ul");
+  ul1.className = 'row'
   let row = hash[index1];
   let index2 = 0;
   while (index2 < row.length) {
+
     let li1 = document.createElement("li");
     li1.className = 'li'
     li1.id = row[index2]
     li1.textContent = row[index2];
+
+    //创建按钮
     let button1 = document.createElement("button");
-    button1.textContent = "编辑";
+    button1.textContent = "edit";
     button1.className = row[index2];
     button1.onclick = function(a) {
       let key = a.target.className;
@@ -32,11 +35,12 @@ while (index1 < hash.length) {
       webs[key] = web;
       localStorage.setItem("webs", JSON.stringify(webs));
     };
+
     li1.appendChild(button1);
-    ol1.appendChild(li1);
+    ul1.appendChild(li1);
     index2 += 1;
   }
-  nav.appendChild(ol1);
+  nav.appendChild(ul1);
   index1 += 1;
 }
 
